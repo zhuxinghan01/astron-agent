f7775c648e90c15f8ec8c6edf228e77e7853e4da
yun-zhi-ztl <15071461069@163.com>
2025-09-28 17:33:04 +0800

    feat: add create-user-app

--- console/backend/commons/src/main/java/com/iflytek/astron/console/commons/service/user/AppMstService.java
+++ console/backend/commons/src/main/java/com/iflytek/astron/console/commons/service/user/AppMstService.java
@@ -4,4 +4,7 @@ package com.iflytek.astron.console.commons.service.user;
  * @author yun-zhi-ztl
  */
 public interface AppMstService {
+    boolean exist(String appName);
+
+    void insert(String uid, String appId, String appName, String appDescribe, String apiKey, String apiSecret);
 }
--- console/backend/hub/src/main/java/com/iflytek/astron/console/hub/service/publish/impl/PublishApiServiceImpl.java
+++ console/backend/hub/src/main/java/com/iflytek/astron/console/hub/service/publish/impl/PublishApiServiceImpl.java
@@ -1,8 +1,16 @@
 package com.iflytek.astron.console.hub.service.publish.impl;
 
+import com.iflytek.astron.console.commons.constant.ResponseEnum;
+import com.iflytek.astron.console.commons.exception.BusinessException;
+import com.iflytek.astron.console.commons.service.user.AppMstService;
+import com.iflytek.astron.console.commons.util.RequestContextUtil;
+import com.iflytek.astron.console.hub.dto.publish.CreateAppVo;
+import com.iflytek.astron.console.hub.dto.user.TenantAuth;
 import com.iflytek.astron.console.hub.service.publish.PublishApiService;
+import com.iflytek.astron.console.hub.service.publish.TenantService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -12,5 +20,31 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class PublishApiServiceImpl implements PublishApiService {
-    
+
+    @Autowired
+    private AppMstService appMstService;
+
+    @Autowired
+    private TenantService tenantService;
+
+    @Override
+    public Boolean createApp(CreateAppVo createAppVo) {
+        String uid = RequestContextUtil.getUID();
+
+        if (appMstService.exist(createAppVo.getAppName())) {
+            throw new BusinessException(ResponseEnum.USER_APP_NAME_REPEAT);
+        }
+
+        String appId = tenantService.createApp(uid, createAppVo.getAppName(), createAppVo.getAppDescribe());
+        if (appId == null) {
+            throw new BusinessException(ResponseEnum.USER_APP_ID_CREATE_ERROR);
+        }
+        TenantAuth tenantAuth = tenantService.getAppDetail(appId);
+        if (tenantAuth == null) {
+            throw new BusinessException(ResponseEnum.USER_APP_ID_CREATE_ERROR);
+        }
+        appMstService.insert(uid, appId, createAppVo.getAppName(), createAppVo.getAppDescribe(), tenantAuth.getApiKey(), tenantAuth.getApiSecret());
+
+        return true;
+    }
 }
