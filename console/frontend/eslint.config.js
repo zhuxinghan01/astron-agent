c5ce98a1c653e0bf206c2499e0f9cde03db4ae75
slqcode <songlanqing2010@gmail.com>
2025-09-28 09:54:55 +0800

    chore: update ESLint configuration to include prettier integration and remove redundant extends

--- console/frontend/eslint.config.js
+++ console/frontend/eslint.config.js
@@ -6,6 +6,7 @@ import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default [
   js.configs.recommended,
+  eslintConfigPrettier,
   {
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
@@ -38,7 +39,6 @@ export default [
         fetch: 'readonly',
       },
     },
-    extends: [eslintConfigPrettier],
     plugins: {
       '@typescript-eslint': tseslint,
       prettier: prettier,
