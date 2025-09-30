ba58831d25ba1982f8266371d896a93413fcc6ad
snoopyYang <863978151@qq.com>
2025-09-30 15:46:24 +0800

    feat: optimize page of app management

--- console/frontend/src/components/workflow/tips/select-node/index.tsx
+++ console/frontend/src/components/workflow/tips/select-node/index.tsx
@@ -1,32 +1,32 @@
-import React from 'react'
-import cloneDeep from 'lodash/cloneDeep'
-import { message } from 'antd'
+import React from 'react';
+import cloneDeep from 'lodash/cloneDeep';
+import { message } from 'antd';
 
-export default function Select({
-  lastSelection
-}) {
-
+export default function Select({ lastSelection }) {
   const copyNodes = async () => {
-    const cloneLastSelection = cloneDeep(lastSelection)
-    cloneLastSelection.nodes = cloneLastSelection.nodes?.filter(node => node.type !== '开始节点' && node.type !== '结束节点')
+    const cloneLastSelection = cloneDeep(lastSelection);
+    cloneLastSelection.nodes = cloneLastSelection.nodes?.filter(
+      node => node.type !== '开始节点' && node.type !== '结束节点'
+    );
     try {
       await navigator.clipboard.writeText(JSON.stringify(cloneLastSelection));
-      message.success('复制成功')
+      message.success('复制成功');
     } catch (err) {
-      console.error("[Clipboard] 复制失败", err);
+      console.error('[Clipboard] 复制失败', err);
     }
-  }
+  };
 
   return (
-    <div className='fixed top-[100px] left-[50%] translate-x-[-50%] z-50 flex items-center gap-2'>
-      <div className='border-[#275EFF] px-4 py-2 rounded-md bg-[#fff]'>
+    <div className="fixed top-[100px] left-[50%] translate-x-[-50%] z-50 flex items-center gap-2">
+      <div className="border-[#275EFF] px-4 py-2 rounded-md bg-[#fff]">
         {`已选中${lastSelection?.nodes?.length}个节点`}
       </div>
-      <div className='px-4 py-2 rounded-md bg-[#275EFF] text-white cursor-pointer'
+      <div
+        className="px-4 py-2 rounded-md bg-[#275EFF] text-white cursor-pointer"
         onClick={() => copyNodes()}
       >
         复制
       </div>
     </div>
-  )
+  );
 }
