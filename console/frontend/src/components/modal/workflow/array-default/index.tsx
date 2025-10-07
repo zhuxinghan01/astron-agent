006b96ddf20b67de13a310b79a0b3f56ff02bf8c
woic <woic.wz@gmail.com>
2025-10-07 14:59:44 +0800

    feat: Remove the redundant console.log statements and unnecessary attributes

--- console/frontend/src/components/modal/workflow/array-default/index.tsx
+++ console/frontend/src/components/modal/workflow/array-default/index.tsx
@@ -44,8 +44,6 @@ const ArrayDefault: FC<{
     setDefaultParamsData,
   });
 
-  console.log(defaultParamsData, 'defaultParamsData');
-
   return (
     <div className="mask">
       <div className="modalContent min-w-[624px] flex flex-col min-h-[350px] pr-0">
