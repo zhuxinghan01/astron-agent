a55c394a3b7d9e6c7d26ca936c09ddd9c591adbb
slqcode <songlanqing2010@gmail.com>
2025-10-08 10:50:44 +0800

    chore: Update dependencies in pnpm-lock.yaml and refactor login redirection function in http.ts

--- console/frontend/src/utils/http.ts
+++ console/frontend/src/utils/http.ts
@@ -125,7 +125,7 @@ export const downloadFileWithHeaders = (
 /**
  * 跳转到登录页
  */
-export const jumpTologin = (): void => {
+export const redirectToLogin = (): void => {
   handleLoginRedirect();
   // eventBus.emit('openLoginModal');
 };
@@ -149,6 +149,7 @@ export const initServerError = (
   }
   // 判断如果是401错误 直接跳转至登录页
   if (response?.status === 401) {
+    redirectToLogin();
     return {
       code: 101,
       message: '尚未登录，请重新登录',
@@ -185,13 +186,13 @@ export const initBusinessError = (
     });
     // 登陆异常
     if (!specialRouter.includes(window.location.pathname)) {
-      jumpTologin();
+      redirectToLogin();
     }
     if (
       response.config.url &&
       specialRequestUrl.includes(response.config.url)
     ) {
-      jumpTologin();
+      redirectToLogin();
     }
   }
   if (
@@ -446,7 +447,6 @@ const getBaseURL = (): string => {
 };
 
 export const baseURL = getBaseURL();
-
 
 axios.defaults.baseURL = baseURL;
 export default axios;
