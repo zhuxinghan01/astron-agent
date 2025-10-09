b0d3918dd2da88e74af82ee84db13445460a1c00
yun-zhi-ztl <15071461069@163.com>
2025-10-09 10:54:18 +0800

    feat: add create time for get api list

--- console/backend/hub/src/main/java/com/iflytek/astron/console/hub/dto/publish/AppListDTO.java
+++ console/backend/hub/src/main/java/com/iflytek/astron/console/hub/dto/publish/AppListDTO.java
@@ -1,10 +1,14 @@
 package com.iflytek.astron.console.hub.dto.publish;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
 
 /**
  * @author yun-zhi-ztl
@@ -30,4 +34,9 @@ public class AppListDTO {
 
     @Schema(description = "App Secret")
     private String appSecret;
+
+    @Schema(description = "create time")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private LocalDateTime createTime;
 }
