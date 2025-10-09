6442b292b78ffccd360cf80770b121ffed0d54d1
mingsuiyongheng <1653497380@qq.com>
2025-10-09 10:11:43 +0800

    feat: Add author comment to Share

--- console/backend/hub/src/main/java/com/iflytek/astron/console/hub/data/impl/ShareDataServiceImpl.java
+++ console/backend/hub/src/main/java/com/iflytek/astron/console/hub/data/impl/ShareDataServiceImpl.java
@@ -8,6 +8,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+/**
+ * @author mingsuiyongheng
+ */
 @Service
 @Slf4j
 public class ShareDataServiceImpl implements ShareDataService {
