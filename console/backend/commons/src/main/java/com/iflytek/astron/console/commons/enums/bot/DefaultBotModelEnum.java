c8801903974bcc89853ac655b6bbb1f554ecf131
mingsuiyongheng <1653497380@qq.com>
2025-10-13 16:52:10 +0800

    feat: Add an icon URL for the DeepSeek model

--- console/backend/commons/src/main/java/com/iflytek/astron/console/commons/enums/bot/DefaultBotModelEnum.java
+++ console/backend/commons/src/main/java/com/iflytek/astron/console/commons/enums/bot/DefaultBotModelEnum.java
@@ -7,8 +7,8 @@ public enum DefaultBotModelEnum {
 
     X1("星火大模型 Spark X1", "x1", "https://openres.xfyun.cn/xfyundoc/2025-09-24/e9b74fbb-c2d6-4f4a-8c07-0ea7f03ee03a/1758681839941/icon.png"),
     SPARK_4_0("星火大模型 Spark V4.0 Ultra", "spark", "https://openres.xfyun.cn/xfyundoc/2025-09-24/e9b74fbb-c2d6-4f4a-8c07-0ea7f03ee03a/1758681839941/icon.png"),
-    DEEPSEEK_V3("DeepSeek-V3", "deepseek-chat", ""),
-    DEEPSEEK_R1("DeepSeek-R1", "deepseek-reasoner", "");
+    DEEPSEEK_V3("DeepSeek-V3", "deepseek-chat", "https://openres.xfyun.cn/xfyundoc/2025-10-13/ccc37b07-5f50-419b-b92c-d1147b8a640a/1760345450309/%E5%BE%AE%E4%BF%A1%E5%9B%BE%E7%89%87_20251013165000_27.png"),
+    DEEPSEEK_R1("DeepSeek-R1", "deepseek-reasoner", "https://openres.xfyun.cn/xfyundoc/2025-10-13/ccc37b07-5f50-419b-b92c-d1147b8a640a/1760345450309/%E5%BE%AE%E4%BF%A1%E5%9B%BE%E7%89%87_20251013165000_27.png");
 
     private String name;
     private String domain;
