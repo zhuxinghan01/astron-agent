4ffec09d5f8e283a47fd7206c03c8bfec99f18be
clliu19 <clliu19@iflytek.com>
2025-10-14 21:25:00 +0800

    !91 fix: repo and flow list fixed * Merge branch 'bd_build' into bd_build_lcl * Merge branch 'feature/workflow-tool' into bd_build_lcl * fix: repo and flow list fixed * Merge branch 'feature/workflow-tool' into bd_build_lcl * Merge branch 'bd_build' into bd_build_lcl * fix: bug fixed * feat: sql fixed * Merge branch 'feature/workflow-tool' into bd_build_lcl * feat: sql fixed * merge code form bd-buid * feat: code merge form main * feat: code merge form main * Merge branch 'bd_build' into feature/workflow-tool * fix: spaceid filter add * Merge branch 'bd_build' into feature/workflow-tool

--- console/backend/toolkit/src/main/java/com/iflytek/astron/console/toolkit/service/workflow/WorkflowService.java
+++ console/backend/toolkit/src/main/java/com/iflytek/astron/console/toolkit/service/workflow/WorkflowService.java
@@ -167,7 +167,7 @@ public class WorkflowService extends ServiceImpl<WorkflowMapper, Workflow> {
     public static final String CLONED_SUFFIX_PATTERN = "[(]\\d+[)]$";
 
     private static final String JSON_KEY_BOT_ID = "botId";
-    private static final String PUBLISH_SUCCESS = "Success";
+    private static final String PUBLISH_SUCCESS = "成功";
     private static final int DEFAULT_ORDER = 0;
 
     @Value("${spring.profiles.active}")
