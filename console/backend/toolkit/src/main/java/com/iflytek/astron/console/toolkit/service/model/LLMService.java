ee6ad6391b95a65ab4992e873c3d7321d6deba8b
clliu19 <clliu19@iflytek.com>
2025-10-14 17:57:56 +0800

    !85 fix: sql fixed * feat: sql fixed * Merge branch 'feature/workflow-tool' into bd_build_lcl * feat: sql fixed * merge code form bd-buid * feat: code merge form main * feat: code merge form main * Merge branch 'bd_build' into feature/workflow-tool * fix: spaceid filter add * Merge branch 'bd_build' into feature/workflow-tool * fix: SparkApiTool appid changed * Merge remote-tracking branch 'upstream/main' into feature/workflow-tool * fix: check code fix and config upgrade

--- console/backend/toolkit/src/main/java/com/iflytek/astron/console/toolkit/service/model/LLMService.java
+++ console/backend/toolkit/src/main/java/com/iflytek/astron/console/toolkit/service/model/LLMService.java
@@ -227,8 +227,8 @@ public class LLMService {
         if (spaceId != null) {
             lambdaQueryWrapper.eq(Model::getSpaceId, spaceId);
         } else {
-            lambdaQueryWrapper.eq(Model::getUid, userId);
-            lambdaQueryWrapper.eq(Model::getSpaceId, null);
+            lambdaQueryWrapper.eq(Model::getUid, userId)
+                    .isNull(Model::getSpaceId);
         }
         List<Model> models = modelMapper.selectList(lambdaQueryWrapper);
 
