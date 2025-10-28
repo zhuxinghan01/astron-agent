522ccc62d5e66660b2ca0ec6aadced5349c224f1
zxh <zhuxinghan01@163.com>
2025-10-28 14:10:09 +0800

    feat: makefile check java code

--- console/backend/commons/src/main/java/com/iflytek/astron/console/commons/dto/bot/TalkAgentCreateDto.java
+++ console/backend/commons/src/main/java/com/iflytek/astron/console/commons/dto/bot/TalkAgentCreateDto.java
@@ -1,9 +1,10 @@
 package com.iflytek.astron.console.commons.dto.bot;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
 public class TalkAgentCreateDto extends BotCreateForm {
     private TalkAgentConfigDto talkAgentConfig;
-
-    public TalkAgentConfigDto getTalkAgentConfig() {
-        return this.talkAgentConfig;
-    }
 }
--- console/backend/commons/src/main/java/com/iflytek/astron/console/commons/dto/bot/TalkAgentUpgradeDto.java
+++ console/backend/commons/src/main/java/com/iflytek/astron/console/commons/dto/bot/TalkAgentUpgradeDto.java
@@ -1,10 +1,10 @@
 package com.iflytek.astron.console.commons.dto.bot;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
+@Data
+@EqualsAndHashCode(callSuper = true)
 public class TalkAgentUpgradeDto extends TalkAgentCreateDto {
     private Integer sourceId;
-
-    public Integer getSourceId() {
-        return this.sourceId;
-    }
 }
